a364bcf65a827558548ac0acbfe65ace7e425aba
sumukhi0 <80339955+sumukhi0@users.noreply.github.com>
2021-06-15 11:55:52 +0530

    Add files via upload

---
+++ js/Quiz.js
@@ -0,0 +1,71 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+    //write code here to hide question elements
+    question.hide();
+
+    //write code to change the background color here
+    background("yellow");
+    //write code to show a heading for showing the result of Quiz
+    fill(0)
+    textSize(50)
+    text("Results of the questions",340,50);
+    text("....................",320,65);
+    
+    //call getContestantInfo( ) here
+    Contestant.getPlayerInfo();
+
+    //write condition to check if contestantInfor is not undefined
+    //write code to add a note here
+    if(allContestants !== undefined){
+      debugger;
+      var display_Answers = 230;
+      fill("blue");
+      textSize(20);
+      text("*Note: Contestant who answered correct are highlighted in green color!",130,230);
+    }
+
+    //write code to highlight contest who answered correctly
+    for(var plr in allContestants){
+      var correctAns = "2";
+      if(correctAns === allContestants[plr].answer)
+      fill("green");
+      else
+      fill("red");
+
+      display_Answers +=30;
+      textSize(30);
+      text(allContestants[plr].name+":"+ allContestants[plr].answer,250,display_Answers);
+    }
+    
+  }
+
+}
